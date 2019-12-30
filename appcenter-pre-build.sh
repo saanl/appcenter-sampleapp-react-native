deca0c4e71aa01d69512a70457a4256ad625f227
Will Liu <v-wiliu@microsoft.com>
2019-12-30 16:06:54 +0800

    add script

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# For Android apps, update the contents of the google-service.json file.
+# This can be used for an app that is open source, or to have a different configuration
+# for different branches.
+#
+# Suppose in our project exists two branches: master and develop. 
+# We can release an app for production push notifications from the master branch 
+# and a version of the app for test push notifications from the develop branch. 
+# We just need configure this behaviour with environment variable in each branch :)
+#
+# DECLARE THE GOOGLE_SERVICES_JSON ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION, SET
+# TO THE CONTENTS OF YOUR google-services.json FILE
+
+echo "##########################NPM INSTALL###########################"
+
+echo "appcenter-pre script from package level"
+
+
+
+
